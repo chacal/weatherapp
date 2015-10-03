a63b581495a428a5943cf61ead5e06b294640ee5
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2015-10-03 16:38:58 +0300

    Use compression on server side

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 var express = require('express')
+var compression = require('compression')
 
 var app = express()
 app.set('port', (process.env.PORT || 8005))
 
 console.log("Starting weatherapp..")
 
+app.use(compression())
 app.use(express.static(__dirname + '/../public'))
 
 app.listen(app.get('port'), function() {
