f6a0b8167b6d7f83f3b9ce785c6f857dd1ce3012
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-03-20 09:26:55 +0200

    Types for main.ts

--- client/main.ts
+++ client/main.ts
@@ -1,25 +1,30 @@
+import EventStream = Bacon.EventStream
+import Bacon = require('baconjs')
+import $ = require('jquery')
+import moment = require('moment')
+
 var modernizr = require('exports-loader?window.Modernizr!./modernizr-custom')
-var googleMaps = require('google').maps
-var Bacon = require('baconjs')
-var $ = require('jquery')
-var moment = require('moment')
 var _ = require('lodash')
 
 import NavigationSlider from './NavigationSlider'
 import initBgMap from './BackgroundMap'
+import ForecastRendering from './ForecastRendering'
+import {AreaForecast, PointForecast, Coords, ForecastItem} from "./ForecastDomain"
+import Property = Bacon.Property
+declare module 'baconjs' {
+  function fromEvent<E, A>(target: noUiSlider.noUiSlider, eventName: string, eventTransformer: (t: number[], m: number) => A): Bacon.EventStream<E, A>;
+}
 
 require('normalize.css')
 require('../node_modules/nouislider/distribute/nouislider.min.css')
 require('../public/css/main.css')
 
-import ForecastRendering from './ForecastRendering'
-
 const HOURS_PER_SLIDER_STEP = 3
-var currentLocation = {lat: 60, lng: 25}
+const currentLocation = {lat: 60, lng: 25}
+const fmiProxyUrl = 'https://www.tuuleeko.fi/fmiproxy'
 
 const map = initBgMap(currentLocation)
 const navigationSlider = new NavigationSlider('slider', HOURS_PER_SLIDER_STEP)
-const fmiProxyUrl = 'https://www.tuuleeko.fi/fmiproxy'
 
 initializeNavigationButtons()
 initializeInfoButton()
@@ -27,13 +32,13 @@ initializeEventStreams()
 initializeForecastTimePanel()
 
 
-function initializeNavigationButtons() {
-  var $navigationContainer = $(`<div id="navigationContainer">
+function initializeNavigationButtons(): void {
+  const $navigationContainer = $(`<div id="navigationContainer">
     <button id="previousForecast" class="navigationButton mapControl"><</button>
     <button id="nextForecast" class="navigationButton mapControl">></button>
   </div>`)
 
-  map.controls[googleMaps.ControlPosition.RIGHT_BOTTOM].push($navigationContainer.get(0))
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push($navigationContainer.get(0))
 
   const buttonEvent = modernizr.touchevents ? 'touchend' : 'click'
   $('#map').on(buttonEvent, '#previousForecast', () => {
@@ -44,9 +49,9 @@ function initializeNavigationButtons() {
 }
 
 
-function initializeInfoButton() {
-  var infoButton = $(`<button id="infoButton" class="mapControl">i</button>`)
-  map.controls[googleMaps.ControlPosition.LEFT_TOP].push(infoButton.get(0))
+function initializeInfoButton(): void {
+  const infoButton = $(`<button id="infoButton" class="mapControl">i</button>`)
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(infoButton.get(0))
 
   $('#map').on('click', '#infoButton', showInfoPopup)
 
@@ -59,32 +64,32 @@ function initializeInfoButton() {
 }
 
 
-function initializeEventStreams() {
+function initializeEventStreams(): void {
   const forecastRendering = new ForecastRendering(map)
-  const boundsChanges = Bacon.fromEvent(map, 'idle', () => map.getBounds())
+  const boundsChanges: EventStream<any, google.maps.LatLngBounds> = Bacon.fromEvent(map as any, 'idle').map(() => map.getBounds())
 
   // Render wind markers when map bounds change
   boundsChanges
     .flatMapLatest(getAreaForecast)
-    .map('.pointForecasts')
+    .map(af => af.pointForecasts)
     .filter(pointForecasts => pointForecasts.length > 0)
     .map(pointForecasts => {
       const availableForecastItems = pointForecasts[0].forecastItems.length
       return { forecasts: pointForecasts, slider: navigationSlider.initialize(availableForecastItems - 1) }
     })
-    .flatMapLatest(forecastAndSlider => {
-      return Bacon.once(parseInt(forecastAndSlider.slider.get()))
-        .merge(sliderChanges(forecastAndSlider.slider))
-        .map(sliderValue => ({ forecasts: forecastAndSlider.forecasts, sliderValue: sliderValue }))
+    .flatMapLatest(({forecasts, slider}) => {
+      return Bacon.once(slider.get() as number)
+        .merge(sliderChanges(slider))
+        .map(sliderValue => ({ forecasts, sliderValue }))
     })
-    .onValue(forecastAndSliderValue => {
-      forecastRendering.renderSelectedForecastItems(forecastAndSliderValue.forecasts, forecastAndSliderValue.sliderValue)
-      updateForecastTime(forecastAndSliderValue.forecasts, forecastAndSliderValue.sliderValue)
+    .onValue(({forecasts, sliderValue}) => {
+      forecastRendering.renderSelectedForecastItems(forecasts, sliderValue)
+      updateForecastTime(forecasts, sliderValue)
     })
 
-  const mapClicks = function() {  // Only for namespacing, called immediately
-    const delayedClicks = Bacon.fromEvent(map, 'click').delay(200)
-    const dblClicks = Bacon.fromEvent(map, 'dblclick').map(() => 'dblClick')
+  const mapClicks: Property<any, google.maps.MouseEvent> = function() {  // Only for namespacing, called immediately
+    const delayedClicks = Bacon.fromEvent(map as any, 'click').delay(200)
+    const dblClicks = Bacon.fromEvent(map as any, 'dblclick').map('dblClick')
     return delayedClicks.merge(dblClicks)
       .slidingWindow(2)
       .filter(events => !_.includes(events, 'dblClick'))  // Click happens only if double click has not happened
@@ -96,8 +101,7 @@ function initializeEventStreams() {
   mapClicks
     .map(e => e.latLng)
     .onValue(latLng => {
-      var startTime = encodeURIComponent(moment().format())
-      var forecastItemsE = Bacon.fromPromise($.get(`${fmiProxyUrl}/hirlam-forecast?lat=${latLng.lat()}&lon=${latLng.lng()}&startTime=${startTime}`))
+      const forecastItemsE: EventStream<any, ForecastItem[]> = getPointForecast(latLng)
         .map(pointForecast => _.dropWhile(pointForecast.forecastItems, item => new Date(item.time).getHours() % HOURS_PER_SLIDER_STEP !== 0).filter((item, idx) => idx % HOURS_PER_SLIDER_STEP === 0))
       forecastRendering.showPointForecastPopup(forecastItemsE)
     })
@@ -111,26 +115,32 @@ function initializeEventStreams() {
   })
 
 
-  function sliderChanges(slider) {
-    return sliderEvents(slider, 'slide').debounceImmediate(300).merge(sliderEvents(slider, 'set')).skipDuplicates()
+  function sliderChanges(slider: noUiSlider.noUiSlider) {
+    return sliderValues('slide').debounceImmediate(300).merge(sliderValues('set')).skipDuplicates()
+
+    function sliderValues(eventName: string): EventStream<any, number> {
+      return Bacon.fromEvent(slider, eventName, (values: number[], handle: number) => values[handle])
+    }
   }
 
-  function sliderEvents(slider, eventName) {
-    return Bacon.fromEvent(slider, eventName, (values, handle) => values[handle])
-  }
 
-  function getAreaForecast(bounds) {
-    var boundsParam = [bounds.getSouthWest().lat(), bounds.getSouthWest().lng(), bounds.getNorthEast().lat(), bounds.getNorthEast().lng()].join(',')
-    var startTime = encodeURIComponent(moment().format())
+  function getAreaForecast(bounds: google.maps.LatLngBounds): EventStream<any, AreaForecast> {
+    const boundsParam = [bounds.getSouthWest().lat(), bounds.getSouthWest().lng(), bounds.getNorthEast().lat(), bounds.getNorthEast().lng()].join(',')
+    const startTime = encodeURIComponent(moment().format())
     return Bacon.fromPromise($.get(`${fmiProxyUrl}/hirlam-forecast?bounds=${boundsParam}&startTime=${startTime}`))
   }
 
-  function updateForecastTime(forecasts, forecastItemIndex) {
+  function getPointForecast(coords: google.maps.LatLng): EventStream<any, PointForecast> {
+    const startTime = encodeURIComponent(moment().format())
+    return Bacon.fromPromise($.get(`${fmiProxyUrl}/hirlam-forecast?lat=${coords.lat()}&lon=${coords.lng()}&startTime=${startTime}`))
+  }
+
+  function updateForecastTime(forecasts: PointForecast[], forecastItemIndex: number) {
     $('#renderedTime').empty().append(moment(forecasts[0].forecastItems[forecastItemIndex].time).format("ddd HH:mm"))
   }
 }
 
-function initializeForecastTimePanel() {
-  var $renderedTime = $(`<div id="renderedTime" class="mapControl">-</div>`)
-  map.controls[googleMaps.ControlPosition.TOP_CENTER].push($renderedTime.get(0))
+function initializeForecastTimePanel(): void {
+  const $renderedTime = $(`<div id="renderedTime" class="mapControl">-</div>`)
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push($renderedTime.get(0))
 }
