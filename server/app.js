5a37533b64df04a421b9605d4224d1a91017c7d4
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2021-04-24 09:51:05 +0300

    Cleanup old implementation & update dependencies

--- server/app.js
+++ server/app.js
@@ -1,24 +1,19 @@
 var express = require('express')
 var compression = require('compression')
-var morgan = require('morgan')
-var logging = require('./logging.js')
-var logger = logging.console
 
 var app = express()
 app.set('port', (process.env.PORT || 8005))
-app.use(morgan(logging.requestLoggingFormat, { stream: logging.fileLoggerStream }))
 
-logger.info("Starting weatherapp..")
+console.log('Starting weatherapp..')
 
 app.use(compression())
 app.use(express.static(__dirname + '/../public'))
 
-app.listen(app.get('port'), function() {
-  logger.info("Weather app is running at localhost:" + app.get('port'))
+app.listen(app.get('port'), function () {
+  console.log('Weather app is running at localhost:' + app.get('port'))
 })
 
 app.use(function (err, req, res, next) {
-  logger.error(err)
   res.status(err.status || 500)
   res.json({
     message: err.message,
